2bbce73e3aaf4866c21693277331217e1cb05f6f
+ <khalifnashon@gmail.com>
2023-04-15 01:08:48 +0300

    done

--- src/reducer.js
+++ src/reducer.js
@@ -1,9 +1,12 @@
-let state = { count: 0 };
+// we declare but do not assign our state, so it starts off undefined. 
+// Then at the bottom the file, we dispatch an action of '@@INIT'. 
+let state;
 
-function changeState(state, action) {
+function changeState(state = { count: 0 }, action) {
   switch (action.type) {
     case "counter/increment":
       return { count: state.count + 1 };
+
     default:
       return state;
   }
@@ -15,6 +18,7 @@ function dispatch(action) {
 }
 
 function render() {
-  const app = document.querySelector("#app");
-  app.textContent = state.count;
+  document.body.textContent = state.count;
 }
+
+dispatch({ type: "@@INIT" });
